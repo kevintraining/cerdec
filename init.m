f089095321e48ec8234dd54dd88a776b2b3e2332
kevintraining <57758428+kevintraining@users.noreply.github.com>
2020-11-09 13:30:44 -0500

    Fix bug.

--- init.m
+++ init.m
@@ -4,6 +4,7 @@ addpath c:\winmerge\winmerge-master
 
 %% Do a dummy commit to cache username and password.
 writecell({'dummy file'}, 'c:\github\trunk\dummy.txt')
+svn add c:\github\trunk\dummy.txt
 svn ci c:\github\trunk\dummy.txt -m "Test" --username kevintraining --password CYf35T_8vn
 svn rm c:\github\trunk\dummy.txt
 svn ci c:\github\trunk\dummy.txt -m "Delete test file."
